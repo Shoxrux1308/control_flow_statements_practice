349605d699a4dc75173d4b8d97ed70e08cb60daa
Shoxrux1308 <shohruxb1308@gmail.com>
2024-03-05 16:51:35 +0500

    123

--- task_max.py
+++ task_max.py
@@ -1,15 +1,22 @@
-def find_max(a,b,c):
-    """
-    Find the maximum number.
-    Args:
-
-        a: int
-        b: int
-        c: int
-    return:
-        int
-    """
-    return 0
+#def find_max(a,b,c):
+ #   """
+ #   Find the maximum number.
+ #   Args:
+#
+ #       a: int
+ #       b: int
+ #       c: int
+ #   return:
+ #       int
+ #   """
+ #   if a<c and b<c:
+ #     return c
+ #   if a<b and c<b:
+ #      return b
+ #   if a>c and a>b:
+#       return b 
+#print(find_max(1,2,3))
+#print(find_max(-1,12,3))
 
 # Example:
 # find_max(1,2,3) -> 3
@@ -25,7 +32,15 @@ def find_max_idx(a,b,c):
     return:
         int
     """
-    return 0
+    if a<c and b<c:
+      return 3
+    if a<b and c<b:
+       return 2
+    if a>c and a>b:
+       return 1
+print(find_max_idx(10,2,13))
+print(find_max_idx(-1,12,3))
+
 
 # Example:
 # find_max_idx(10,2,13) -> 3
